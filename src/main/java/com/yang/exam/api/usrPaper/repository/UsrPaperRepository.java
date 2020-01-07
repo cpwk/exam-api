b3838b4466b4ce36630fd4259200d31deb3ff98a
杨成成 <861746159@qq.com>
2020-01-07 09:39:28 +0800

    first commit

---
+++ src/main/java/com/yang/exam/api/usrPaper/repository/UsrPaperRepository.java
@@ -0,0 +1,13 @@
+package com.yang.exam.api.usrPaper.repository;
+
+import com.yang.exam.api.usrPaper.model.UsrPaper;
+import com.yang.exam.commons.reposiotry.BaseRepository;
+
+/**
+ * @author: yangchengcheng
+ * @Date: 2019/12/11 19:11
+ * @Version：1.0
+ */
+public interface UserPaperRepository extends BaseRepository<UsrPaper, Integer> {
+
+}
